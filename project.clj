f9a24ade996936e5ec1b87705bfcfb005228020a
Felix Flores <felixflores@gmail.com>
2014-10-14 12:18:41 -0400

    update project reps

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2342"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [om "0.7.3"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
