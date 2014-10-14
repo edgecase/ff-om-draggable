70d5feaa3ae42907b98bb4b98e712a79fe376319
Felix Flores <felixflores@gmail.com>
2014-10-14 11:01:28 -0400

    initial work to add touch support

--- src/ff_om_draggable/core.cljs
+++ src/ff_om_draggable/core.cljs
@@ -8,18 +8,41 @@
 
 (def position-chan (chan (sliding-buffer 1)))
 (def mouse-mult (mult position-chan))
-(js/window.addEventListener "mousemove"
-                            #(put! position-chan {:left (.-clientX %) :top (.-clientY %)}))
+
+(defn touch-move
+  [e]
+  (.preventDefault e)
+  (let [touch (aget (.-changedTouches e) 0)]
+    {:left (.-clientX touch) :top (.-clientY touch)}))
+
+(js/window.addEventListener "mousemove" #(put! position-chan {:left (.-clientX %) :top (.-clientY %)}))
+(js/window.addEventListener "touchmove" #(put! position-chan (touch-move %)))
 
 (defn target-position
   [e]
   (let [rect (.getBoundingClientRect (.-currentTarget e))]
     {:top (.-top rect) :left (.-left rect)}))
 
+(defn touch-start
+  [e owner]
+  (.preventDefault e)
+  (let [user-movement (om/get-state owner :user-movement)
+        touch (aget (.-changedTouches e) 0)
+        current-position (target-position e)
+        offset {:top (- (.-clientY touch) (current-position :top))
+                :left (- (.-clientX touch) (current-position :left))}
+        new-position (fn [mouse]
+                       {:top (- (mouse :top) (offset :top))
+                        :left (- (mouse :left) (offset :left))})]
+
+    (om/set-state! owner :new-position new-position)
+    (tap mouse-mult user-movement)))
+
+
 (defn move-start
   [e owner]
   (.preventDefault e)
-  (let [mouse-tap (om/get-state owner :mouse-tap)
+  (let [user-movement (om/get-state owner :user-movement)
         current-position (target-position e)
         offset {:top (- (.-clientY e) (current-position :top))
                 :left (- (.-clientX e) (current-position :left))}
@@ -27,13 +50,13 @@
                        {:top (- (mouse :top) (offset :top))
                         :left (- (mouse :left) (offset :left))})]
     (om/set-state! owner :new-position new-position)
-    (tap mouse-mult mouse-tap)))
+    (tap mouse-mult user-movement)))
 
 (defn move-end
   [e owner cursor position-cursor]
   (.preventDefault e)
-  (let [mouse-tap (om/get-state owner :mouse-tap)]
-    (untap mouse-mult mouse-tap)
+  (let [user-movement (om/get-state owner :user-movement)]
+    (untap mouse-mult user-movement)
     (om/update! cursor position-cursor (target-position e))
     (om/set-state! owner :new-position nil)))
 
@@ -50,11 +73,11 @@
       om/IInitState
       (init-state [_]
         {:position (get-in item position-cursor)
-         :mouse-tap (chan)
+         :user-movement (chan)
          :new-position nil})
       om/IWillMount
       (will-mount [_]
-        (let [position (om/get-state owner :mouse-tap)]
+        (let [position (om/get-state owner :user-movement)]
           (go-loop []
             (let [mouse (<! position)
                   new-position (om/get-state owner :new-position)]
@@ -63,6 +86,8 @@
       om/IRenderState
       (render-state [_ state]
         (dom/div (clj->js {:style (conj {:position "absolute"} (position item position-cursor state))
+                           :onTouchStart #(touch-start % owner)
                            :onMouseDown #(move-start % owner)
+                           :onTouchEnd #(move-end % owner item position-cursor)
                            :onMouseUp #(move-end % owner item position-cursor)})
                  (om/build view item))))))
